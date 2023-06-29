69330928a6f9f1838963edc0345f1229b45f5a90
Abdulaziz <abdulazizkomilov2001@gmail.com>
2023-06-29 09:59:11 +0500

    Update urls.py

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,13 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 
 urlpatterns = [
+    re_path(
+        r'^o/(?P<provider>\S+)/$',
+        views.CustomProviderAuthView.as_view(),
+        name='provider-auth'
+    ),
     path('jwt/create/', views.CustomTokenObtainPairView.as_view()),
     path('jwt/refresh/', views.CustomTokenRefreshView.as_view()),
     path('jwt/verify/', views.CustomTokenVerifyView.as_view()),

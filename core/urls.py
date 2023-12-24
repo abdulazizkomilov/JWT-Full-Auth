d3387a386c059478cf2d72d84b6ce60bafab011f
Abdulaziz <abdulazizkomilov2001@gmail.com>
2023-12-25 02:14:24 +0500

    deploy

--- core/urls.py
+++ core/urls.py
@@ -2,9 +2,14 @@ from django.contrib import admin
 from django.http import HttpResponse
 from django.urls import path, include
 
+
+def hi_view(request):
+    return HttpResponse('<center><h1>Hi</h1></center>')
+
+
 urlpatterns = [
     path('admin-panel/', admin.site.urls),
     path('api/', include('djoser.urls')),
     path('api/', include('users.urls')),
-    path('', HttpResponse('<center><h1>Hi</h1></center>')),
+    path('', hi_view),
 ]
